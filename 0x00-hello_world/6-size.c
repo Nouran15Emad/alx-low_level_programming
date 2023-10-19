1a26d30be0425a420ef2bbe3a4766b8cbc201c32
Nouran Emad <nouran1542001@gmail.com>
2023-10-19 14:57:20 -0700

    ${com}

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,9 @@
 int main(void)
 {
 printf("Size of char : %lu byte(s)\n", sizeof(char));
-printf("Size of short : %lu byte(s)\n", sizeof(short));
 printf("Size of int : %lu byte(s)\n", sizeof(int));
-printf("Size of long : %lu byte(s)\n", sizeof(long));
+printf("Size of long int : %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int : %lu byte(s)\n", sizeof(long long int));
 printf("Size of float : %lu byte(s)\n", sizeof(float));
-printf("Size of double : %lu byte(s)\n", sizeof(double));
-printf("Size of long double : %lu byte(s)\n", sizeof(long double));
 return (0);
 }
