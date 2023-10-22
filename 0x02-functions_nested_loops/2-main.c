7412fa4f54c76edc08442d2c975eb0112cbba2b0
Nouran Emad <nouran1542001@gmail.com>
2023-10-21 17:04:37 -0700

    ${com}

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - Call a function that prints alph ten times
+*
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_alphabet_x10 - a function that prints alph ten times
+*
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	       	char a2z = 'a';
+
+		while (a2z <= 'z')
+		{
+			_putchar(a2z);
+			a2z++;
+		}
+
+		_putchar('\n');
+		i++;
+		
+	}
+
+}
