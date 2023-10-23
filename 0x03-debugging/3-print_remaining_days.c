128503ee53206507d9f8ae9a09767ae3bc58245f
Nouran Emad <nouran1542001@gmail.com>
2023-10-23 10:50:58 -0700

    ${com}

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,23 +11,26 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	int totalDays = 0;
-
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
-		if (month > 2)
+		if (month >= 2 && day >= 60)
 		{
-			totalDays += 1;
+			day++;
 		}
-		else if (month == 2 && day == 29)
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-			return;
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-
-	totalDays += (month - 1) * 30 + day;
-
-	printf("Day of the year: %d\n", totalDays);
-	printf("Remaining days: %d\n", 365 - totalDays);
 }
