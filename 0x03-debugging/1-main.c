1563eb9ed370809efda6905e12b164a459664cc0
Nouran Emad <nouran1542001@gmail.com>
2023-10-23 09:05:47 -0700

    ${com}

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,18 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
-
-	/* the part of the code that is causing the output to go into an infinite loop >> no increment*/
-        while (i < 10)
-        {
-                putchar(i);
-		
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+	/**
+	 * the part of the code that is causing the output to go into an infinite loop >> no increment
+	 */
+	while (i < 10)
+	{
+		putchar(i);
+		break;
+	}
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
