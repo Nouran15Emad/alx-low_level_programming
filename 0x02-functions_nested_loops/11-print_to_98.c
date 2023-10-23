dec370be62d1eb204839d4da4de7bad90dc5049a
Nouran Emad <nouran1542001@gmail.com>
2023-10-22 19:22:01 -0700

    ${com}

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,6 +10,7 @@
 
 void print_to_98(int n)
 {
+
 	while (n <= 98)
 	{
 		printf("%d", n);
@@ -18,6 +19,16 @@ void print_to_98(int n)
 			printf(", ");
 		}
 		n++;
+	}
+	printf("\n");
+	while (n > 98)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+		n--;
 	}
 	printf("\n");
 }
