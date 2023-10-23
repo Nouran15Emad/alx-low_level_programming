fef3e1bdefc3621e9dd415af622fd3a58e1c37ba
Nouran Emad <nouran1542001@gmail.com>
2023-10-23 16:10:33 -0700

    ${com}

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard - Print the chess board
+ * @a: pointer to array of length 8 to char value
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
