a0164e3467c5afc99f2aa3847edd28e7b17f2953
Nouran Emad <nouran1542001@gmail.com>
2023-10-22 18:57:47 -0700

    ${com}

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*add - A function that add two integers 
+*@a: An integer
+*@b: An integer
+*Return: An integer the result of summition
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - checks the code 
+*
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int x;
+
+	x = add(15, 5);
+	printf("%d\n", x);
+	return (0);
+}
