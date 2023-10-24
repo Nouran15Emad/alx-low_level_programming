a5c4d4204ac686fb30e0050813e7d730d3b9e63c
Nouran Emad <nouran1542001@gmail.com>
2023-10-23 18:38:15 -0700

    ${com}

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - check the code and calls funcs
+*
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	more_numbers();
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int _isupper(int c);
 void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
+void more_numbers(void);
