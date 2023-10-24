8ccb589fa779b5406f4c7f2b3cf5680566503c8f
Nouran Emad <nouran1542001@gmail.com>
2023-10-23 19:02:03 -0700

    ${com}

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,14 @@
 void print_line(int n)
 {
 
-	int i ;
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 1; i < n; i++)
 	{
 		_putchar('_');
 	}
