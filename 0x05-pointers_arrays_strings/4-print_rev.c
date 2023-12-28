7fbf92c8771c040465f2cf150322fd3ea934fa47
Nouran Emad <nouran1542001@gmail.com>
2023-12-28 14:08:59 -0800

    ${com}

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 #include "2-strlen.c"
 
 /**
-*print_rev - print a string in reverse followed by a new line 
+*print_rev - print a string in reverse followed by a new line
 *@s: pointer to a string
 *
 *Return: Always 0 (Sucess)
@@ -15,7 +15,7 @@ void print_rev(char *s)
 
 	i = _strlen(s);
 
-	while ( i >= 0)
+	while (i > 0)
 	{
 		printf("%c", *(s + i));
 		i--;
