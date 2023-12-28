40aadd563538264153b8691bc01b367cef76ea14
Nouran Emad <nouran1542001@gmail.com>
2023-12-28 12:33:20 -0800

    ${com}

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-*swap_int - Write a function that swaps the values of two integers 
+*swap_int - Write a function that swaps the values of two integers
 *@a: pointer to an integer
 *@b: pointer to an integer
 *
@@ -12,6 +12,7 @@
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
