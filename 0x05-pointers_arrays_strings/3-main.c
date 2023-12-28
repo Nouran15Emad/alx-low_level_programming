a1c30763f77020039424ac5a83e906cb2f162806
Nouran Emad <nouran1542001@gmail.com>
2023-12-28 13:31:15 -0800

    ${com}

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - check the printing func 
+*
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
