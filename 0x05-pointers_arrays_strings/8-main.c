bec62674c0e43fff907a3e3f0bbe25a266e8b5bc
root <root@Nour>
2023-12-31 20:58:14 +0200

    ${com}

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - check the print-array function
+*
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int array[5] = {125, 89, 987, 54, 75};
+
+	print_array(array, 5);
+
+	return (0);
+}
