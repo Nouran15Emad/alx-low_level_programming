502274242095cd0bb3d1d9b546841e823d9faa58
root <root@Nour>
2024-01-15 16:42:07 +0200

    ${com}

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - 
+*
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+*_strcat - concatenates two strings
+*@dest: pointer to a string (destination)
+*@src: pointer to a string (Source)
+*Return: a pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, dest_len, src_len;
+	
+	dest_len = _strlen(dest);
+	src_len = _strlen(src);
+
+	for ( i = 0; i < ((dest_len + src_len)-1) && i != '\0'; i++)
+	{
+		dest[i + dest_len] = src[i];
+		dest[dest_len + src_len] = '\0';
+		
+	}
+	
+
+	return (dest);
+}
