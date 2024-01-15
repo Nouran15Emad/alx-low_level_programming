93c2b0755ad9dda96d2565b8b05b5dca31252c00
root <root@Nour>
2024-01-15 20:41:17 +0200

    ${com}

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,29 +13,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	/*for ( i = 0; s1[i] != '\0' && s2[i] != '\0';)
-	{
-		if (_strlen(s1) > _strlen(s2))
-		{
-			i++;
-		}
-		else if (_strlen(s1) < _strlen(s2))
-		{
-			i--;
-		}
-		else
-		{
-			i = 0;
-		}
-	}*/
-
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
 
-	return s1[i] - s2[i];
-	
-
-	
+	return (s1[i] - s2[i]);
 }
