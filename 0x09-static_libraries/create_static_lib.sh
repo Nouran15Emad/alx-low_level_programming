8120e69f186b28978ff3ee486e52f6959fba3658
root <root@Nour>
2024-02-21 23:54:38 +0200

    ${com}

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-
-
-# Get all the .c files in the current directory
-c_files=$(find . -maxdepth 1 -type f -name "*.c")
 
 # Compile each .c file into an object file
-for file in $c_files; do
+for file in *.c; do
     gcc -c "$file" -o "${file%.c}.o"
 done
 
 # Create the static library
-ar -rc  liball.a *.o
+ar -rc liball.a *.o
+
+# Index the static library
 ranlib liball.a
 
 # Clean up the object files
