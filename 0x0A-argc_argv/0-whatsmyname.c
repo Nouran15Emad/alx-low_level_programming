8e42924599993f20dc6a6bf2ee44df40a69fbb47
root <root@Nour>
2024-03-12 02:01:15 +0200

    ${com}

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+*main - print the prgram name
+*@argc: integer == the count of program arguments
+*@argv: array of pointers to strings (the program arguments)
+*Return: Always 0 (Success)
+*/
 
 int main(int argc, char *argv[])
 {
